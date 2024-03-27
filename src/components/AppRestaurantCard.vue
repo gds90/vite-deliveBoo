328c1d9c092e7e1f30edf94e247afdac214c00eb
albertomandirola <albymandi00@gmail.com>
2024-03-27 18:23:55 +0100

    create ApprestaurantCard

---
+++ src/components/AppRestaurantCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'AppRestaurantCard',
+    props: {
+        restaurant: Object
+    },
+}
+</script>
+<template lang="">
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" :src=""
+            :alt="{{ $restaurant->name }}">
+        <div class="card-body">
+            <h5 class="card-title">{{ restaurant->name }}</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">{{ $restaurant->address }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+<style lang="scss"></style>
