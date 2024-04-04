a5e726e6f149e49a59e6a9eeb0c9ad53d9c2e19c
balbo97 <balbosauro1@gmail.com>
2024-04-04 13:36:12 +0200

    "Add custom layout for PaymentResponse page and update PaymentCard component to navigate to it; add BasicLayout component"
    This commit includes the following changes:
    
    * The `PaymentResponse` page now has a custom layout, which is defined in the new `BasicLayout` component.
    * The `PaymentCard` component has been updated to navigate to the `PaymentResponse` page with a dynamic parameter indicating the success or failure of the payment.
    * The `BasicLayout` component has been added to the project.
    
    Note: The commit message is written in the imperative tense, as is the convention for Git commit messages. It describes the changes made in the code diff without making any assumptions or interpretations.

--- src/pages/PaymentResponse.vue
+++ src/pages/PaymentResponse.vue
@@ -1,15 +1,64 @@
 <script>
 export default {
-    
+    props: {
+        responseObject: {
+            type: Object,
+           
+        }
+    }
 }
 </script>
 
-<template lang="">
-    <div>
-        
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="container-card">
+                    <div class="card" v-if="responseObject.success">
+                        <div class="card-header bg-success d-flex justify-content-center py-4">
+                            <i class="fa-regular fa-circle-check"></i>
+                        </div>
+                        <div class="card-body text-center py-5">
+                            <h1 class="mb-3">Congratulations!</h1>
+                            <h2>Your payment has been successfully processed.</h2>
+                        </div>
+                    </div>
+                    <div class="card" v-else>
+                        <div class="card-header bg-danger d-flex justify-content-center py-4">
+                            <i class="fa-solid fa-triangle-exclamation"></i>
+                        </div>
+                        <div class="card-body text-center py-5">
+                            <h1 class="mb-3">Oops!</h1>
+                            <h2>It seems there was an issue processing your payment. Please check your payment details and try again.</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-<style lang="">
-    
+<style lang="scss" scoped>
+.container-card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Imposta l'altezza al 100% della viewport per centrare verticalmente */
+} 
+
+.card{
+    width: 70%;
+    border: none;
+}
+
+.card-header{
+    font-size: 70px;
+    color: white;
+}
+
+.card-body{
+    height:100%;
+}
+
+
 </style>
